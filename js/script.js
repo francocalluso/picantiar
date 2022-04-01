4bc82175e419f34028f782e7c5a948465098b8dc
LUIZAPROGRAMA <paulino.fernandez95@gmail.com>
2022-04-01 14:43:44 -0300

    1. Agregue un form para registrar nuevos bares. 2. Para hacer eso tuve que invisibilizar el mapa en css (le puse un visibility hidden hasta que veamos el diseño) 3. Ordene el script un poco. 4. Modificiaciones en html, css y script.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
+// CONSTANTES Y VARIABLES
+
 const apiKey = 'pk.eyJ1IjoiZnJhbmNvY2FsbHVzbyIsImEiOiJja3puYml4amQwMWZmMnVtZndnM28xbTI3In0.pkD6omoxsqQEBPDE6pCzxA';
 
 const mymap = L.map('map').setView([-34.6035614,-58.3855543], 13);
+
+let arrayLugares = [];
+
+const LUGAR1 = new lugar ({nombreLugar: "La Trompeta del Tata",
+                          coordenada1: -34.6003048,
+                          coordenada2: -58.4388054,
+                          imagen: "imagen1",
+});
+
+const LUGAR2 = new lugar ({nombreLugar: "sarkis",
+                          coordenada1: -34.5914768,
+                          coordenada2: -58.4383124,
+                          imagen: "imagen2",
+});
+
+const LUGAR3 = new lugar ({nombreLugar: "Burger King",
+                          coordenada1: -34.5904768,
+                          coordenada2: -58.4183124,
+                          imagen: "imagen2",
+});
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -22,6 +45,59 @@ var myIcon = L.icon({
     shadowAnchor: [22, 94]
 });
 
+// FUNCIONES
+
+class lugar{
+    constructor(data){
+        this.nombreLugar = data.nombreLugar;
+        this.coordenada1 = data.coordenada1;
+        this.coordenada2 = data.coordenada2;
+        this.imagen = data.imagen;
+        arrayLugares.push(this);
+        }
+}
+
+arrayLugares.forEach((lugar) => { //Inserta los lugares al HTML 
+    let template = document.createElement("div");
+    let marker = L.marker(([lugar.coordenada1,lugar.coordenada2]), {icon: myIcon}).addTo(mymap);
+
+    template.innerHTML = `
+        <h3>${lugar.nombreLugar}</h3>
+        <img class="imagen" src="assets/tata.png">
+        <br>
+        <button type="button">Ver más</button>
+    `
+
+    marker.bindPopup(template);
+});
+
+function newBar(){    // Registra un bar al array
+
+    bar = document.getElementById("bar-name").value;
+    coordenada1 = document.getElementById("coor-1").value;
+    coordenada2 = document.getElementById("coor-2").value;
+
+    if (bar && coordenada1 && coordenada2){
+        new lugar ({nombreLugar: bar,
+                                      coordenada1: coordenada1,
+                                      coordenada2: coordenada2
+        })
+        console.log(arrayLugares);
+    }    
+}
+
+
+//  COMENTARIOS SUPLENTES//
+
+
+// template.innerHTML = `
+//     <a class= "album" id="release${lugarFinder}">
+//       <p>${lugar.nombreLugar.toUpperCase()}</p>
+//       <img src="imgs/imagen${lugarFinder}.jpeg" width="200" height="200">
+//       <p>${lugar.coordenada}</p>
+//     </a>
+//     `;
+
 
 // const marker = L.marker(([-34.6003048,-58.4388054]), {icon: myIcon}).addTo(mymap);
 
@@ -37,64 +113,3 @@ var myIcon = L.icon({
 // marker.bindPopup(template);
 
 // marker2.bindPopup(template);
-
-
-
-
-///////////////////////////////////////// TEST //////////////////////////////////////
-
-let arrayLugares = [];
-
-class lugar{
-    constructor(data){
-        this.nombreLugar = data.nombreLugar;
-        this.coordenada1 = data.coordenada1;
-        this.coordenada2 = data.coordenada2;
-        this.imagen = data.imagen;
-        arrayLugares.push(this);
-        }
-}
-
-const LUGAR1 = new lugar ({nombreLugar: "La Trompeta del Tata",
-                          coordenada1: -34.6003048,
-                          coordenada2: -58.4388054,
-                          imagen: "imagen1",
-});
-
-const LUGAR2 = new lugar ({nombreLugar: "sarkis",
-                          coordenada1: -34.5914768,
-                          coordenada2: -58.4383124,
-                          imagen: "imagen2",
-});
-
-const LUGAR3 = new lugar ({nombreLugar: "Burger King",
-                          coordenada1: -34.5904768,
-                          coordenada2: -58.4183124,
-                          imagen: "imagen2",
-});
-
-console.log(arrayLugares);
-
-
-arrayLugares.forEach((lugar) => { //Inserta los lugares al HTML 
-    let template = document.createElement("div");
-    // let lugarFinder = arrayLugares.indexOf(lugar) + 1;
-    let marker = L.marker(([lugar.coordenada1,lugar.coordenada2]), {icon: myIcon}).addTo(mymap);
-
-    template.innerHTML = `
-        <h3>${lugar.nombreLugar}</h3>
-        <img class="imagen" src="assets/tata.png">
-        <br>
-        <button type="button">Ver más</button>
-    `
-
-    marker.bindPopup(template);
-});
-
-// template.innerHTML = `
-//     <a class= "album" id="release${lugarFinder}">
-//       <p>${lugar.nombreLugar.toUpperCase()}</p>
-//       <img src="imgs/imagen${lugarFinder}.jpeg" width="200" height="200">
-//       <p>${lugar.coordenada}</p>
-//     </a>
-//     `;
